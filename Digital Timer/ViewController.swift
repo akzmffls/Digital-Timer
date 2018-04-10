e3ecba66d7ffb279bce87a9e2b734d398b01c81b
akzmffls <d7703_14@d7703-14ui-imac.local>
2018-04-10 15:12:48 +0900

    Digital Timer

--- Digital Timer/ViewController.swift
+++ Digital Timer/ViewController.swift
@@ -10,16 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbl: UILabel!
+    @IBOutlet weak var TimerSwich: UISwitch!
+    var myTimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        TimerSwich.setOn(false, animated: true)
+    }
+    
+    func updateTime() {
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss a"
+        lbl.text = formatter.string(from: date)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func timerStrat(_ sender: Any) {
+        if TimerSwich.isOn == true {
+            
+            myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(myTimer) in self.updateTime()
+                
+            })
+        } else {
+            myTimer.invalidate()
+        }
+    
 
 }
 
+}
